2bcd4290919d0142a6117ae4d7f8bcf4917275d4
qixichao <qixichao@163.com>
2024-11-03 02:38:53 +0800

    original edition

--- Agent/Action.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import List, Optional, Dict, Any
-
-
-class Action(BaseModel):
-    name: str = Field(description="Tool name")
-    args: Optional[Dict[str, Any]] = Field(description="Tool input arguments, containing arguments names and values")
